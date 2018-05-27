a26e5b7422670c848f9da9e9308beac137f2bcc6
Li WeiLiang <357485148@qq.com>
2018-05-27 19:26:12 +0800

    some change to re_format file

--- demo/urls.py
+++ demo/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import url 
-from . import tagging_data_view,tagging_data_writefile_view
+from django.conf.urls import url
+from . import tagging_data_view, tagging_data_writefile_view
 
 urlpatterns = [
     url(r'^$', tagging_data_writefile_view.tagging_push),
