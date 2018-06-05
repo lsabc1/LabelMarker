377b242f49b8bf8b53ee1072ee804a1ca33446a4
Li WeiLiang <357485148@qq.com>
2018-06-05 18:40:14 +0800

    v 1.0

--- demo/tagging_data_view.py
+++ demo/tagging_data_view.py
@@ -2,9 +2,10 @@
 from django.shortcuts import render
 from django.views.decorators import csrf
 
-
 # 数据标注页面的view
 # 接收GET请求数据
+
+
 def showtagging_data(request):
     ctx = {}
     if 'title' in request.GET:
@@ -13,41 +14,45 @@ def showtagging_data(request):
 
         text = ""
         tag_name_list = []
-        file_object = open('label_data/label_list.txt', 'r')
+        file_object = open('label_data/label_list.txt', 'r', encoding='utf-8')
         for f in file_object:
-            tag_name_list.append(f.strip())     #取出所有的标签
+            tag_name_list.append(f.strip())  # 取出所有的标签
         file_object.close()
 
         count = 0
-        for tag in tag_name_list:       #根据标签动态生成投票的页面
+        for tag in tag_name_list:  # 根据标签动态生成投票的页面
             text += '<div class="radio"> <label class="form-check-label">'
-            text += '<input type="radio" name="label" value="' + str(tag) + '">'
+            text += '<input type="radio" name="label" value="' + \
+                str(tag) + '">'
             text += str(tag)
             text += '</label>  </div>'
             count += 1
 
         # 放置一个隐藏的输入框，传递title的值到缓冲页面
-        text += "<input name='title' value='" + str(request.GET["title"]) + "'  style='display:none;' ></input>"
+        text += "<input name='title' value='" + \
+            str(request.GET["title"]) + "'  style='display:none;' ></input>"
 
-        ctx['taggingCheck'] = text      #传递生成的投票页面
+        ctx['taggingCheck'] = text  # 传递生成的投票页面
 
         # 统计当前标注情况
-        file_object = open('label_data/labeled_data.txt', 'r')
+        file_object = open('label_data/labeled_data.txt',
+                           'r', encoding='utf-8')
         s = {}
         sum = 0
         LEN = len(tag_name_list)
         for i in tag_name_list:
             s[i] = 0
-        for f in file_object:
-            pair = f.split()
-            s[pair[0].strip()] += 1
-        for i in tag_name_list:
-            sum += s[i]
+        # for f in file_object:
+        #     pair = f.split()
+        #     if pair:
+        #         s[pair[0].strip()] += 1
+        # for i in tag_name_list:
+        #     sum += s[i]
         file_object.close()
-        text = ""  ##用于记录已标注样本个数
+        text = ""  # 用于记录已标注样本个数
         for i in tag_name_list:
             text += '<p>label ' + str(i) + ': ' + str(s[i]) + '</p>'
         text += '<p>Sum: ' + str(sum) + '</p>'
-        ctx['already'] = text       #传递统计结果页面
+        ctx['already'] = text  # 传递统计结果页面
 
     return render(request, "tagging_data.html", ctx)
--- demo/tagging_data_writefile_view.py
+++ demo/tagging_data_writefile_view.py
@@ -1,54 +1,83 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.views.decorators import csrf
-import random
 
 
 #  先将标注写入文件，之后跳转到tagging_cache.html再进行新页面的跳转
 def tagging_push(request):
     ctx = {}
-    # 先将已有的labels存入字典中
-    file_object = open('label_data/labeled_data.txt', 'r')  # 用来把以测试的data加入字典，并删除前面的序号
-    s = set()
-    for f in file_object:
-        pair = f.strip().split()
-        curs = ""
-        for i in range(1, len(pair)):
-            if i > 1:
-                curs += ' '
-            curs += str(pair[i])
-        s.add(curs.strip())
+    # # 先将已有的labels存入字典中
+    # file_object = open('label_data/labeled_data.txt', 'r')  # 用来把已测试的data加入字典，并删除前面的序号
+    # s = set()
+    # for f in file_object:
+    #     pair = f.strip().split()
+    #     curs = ""
+    #     for i in range(1, len(pair)):
+    #         if i > 1:
+    #             curs += ' '
+    #         curs += str(pair[i])
+    #     s.add(curs.strip())
+    # file_object.close()
+    with open('label_data/index.txt', 'r+', encoding='utf-8') as f:  # 查看下一个要标注的地方
+        f.seek(0, 0)
+        index = f.readline()
+        if index:
+            index_list = index.split()
+        else:
+            f.write('1 1')
+            index_list = [1, 1]
 
-    file_object.close()
+    line = int(index_list[0])
+    str_num = int(index_list[1])
 
-    file_object = open('label_data/unlabeled_data.txt', 'r') #从没标记的里面随机选一个
-    all_list = []
-    for f in file_object:
-        all_list.append(f.strip())
-    ln = len(all_list)
-    next_title = all_list[random.randint(0, ln - 1)]
+    with open('label_data/unlabeled_data.txt', encoding='utf-8') as f:  # 找到要标注的句子和字符
+        lines = f.readlines()
+        sentence = lines[line-1].strip()
 
-    if 'label' in request.GET and 'title' in request.GET:       #找到返回的GET里的东西
+
+
+    # file_object = open('label_data/unlabeled_data.txt', 'r') #从没标记的里面随机选一个
+    # all_list = []
+    # for f in file_object:
+    #     all_list.append(f.strip())
+    # ln = len(all_list)
+    # next_title = all_list[random.randint(0, ln - 1)]`
+
+    if 'label' in request.GET and 'title' in request.GET:  # 找到返回的GET里的东西
         print(str(request.GET) + ".....")
         title = str(request.GET['title']).strip()
         label = str(request.GET['label']).strip()
-        if label != None:
-            file_object = open('label_data/labeled_data.txt', 'a')
-            if title in s:
-                print("该title已存在，冲突！")                  #查看是否已经标过
-            else:
-                file_object.write(label + " " + title + "\n")       #把没标过的东西加入已标注
-                s.add(title)
-            file_object.close()
+        title_char = title[-1]
+        if label:
+            with open('label_data/labeled_data.txt',
+                               'a', encoding='utf-8') as file_object:
+                file_object.write(title_char + " " + label + "\n")  # 把没标过的东西加入已标注
+                if str_num == len(sentence):
+                    str_num = 1
+                    line += 1
+                    file_object.write('\n')
+                    if line > len(lines):  
+                        return render(request, "over.html", ctx)
+                else:
+                    str_num += 1
+
+            with open('label_data/index.txt', 'w+') as f:  # 改变下一个要标注的地方
+                f.write(str(line) + ' ' + str(str_num))
         else:
             print('用户未选择label')
 
-    if len(s) >= len(all_list):     #如果完成了所有的标注，跳转到结束页面
-        return render(request, "over.html", ctx)
+    # if len(s) >= len(all_list):  # 如果完成了所有的标注，跳转到结束页面
+    #     return render(request, "over.html", ctx)
 
-    while next_title in s:  # 如果冲突就再选
-        next_title = all_list[random.randint(0, ln - 1)].strip()
+    # while next_title in s:  # 如果冲突就再选
+    #     next_title = all_list[random.randint(0, ln - 1)].strip()
+    with open('label_data/unlabeled_data.txt', encoding='utf-8') as f:
+        lines = f.readlines()
+        next_sentence = lines[line-1]
+        next_str = next_sentence[str_num-1]
+        next_title = next_sentence + '<br/>' + next_str
 
-    ctx['next'] = "<input id='next' value='" + next_title + "' style='display:none;'></input>"
+    ctx['next'] = "<input id='next' value='" + \
+        next_title + "' style='display:none;'></input>"
 
-    return render(request, "tagging_cache.html", ctx)       #将下一个要标注的信息传递出去
+    return render(request, "tagging_cache.html", ctx)  # 将下一个要标注的信息传递出去
